8c5c77d37d4b0892eb945519b1cab2585a5aba48
Alejandro J. Santander <palebluedot@gmail.com>
2019-05-10 21:30:14 -0300

    Minor changes to blockdate test.

--- test/commands/blockdate.test.js
+++ test/commands/blockdate.test.js
@@ -1,11 +1,14 @@
 const cli = require('../utils/cli.js');
 
-test('Result should be 0', async () => {
-  let result = await cli('blockdate', 'infura_mainnet', '1');
-  expect(result.code).toBe(0);
-});
+describe('blockdate command', () => {
 
-test('Stdout should contain "Thu Jul 30 2015"', async () => {
-  let result = await cli('blockdate', 'infura_mainnet', '1');
-  expect(result.stdout).toContain('Thu Jul 30 2015');
+  test('Result code should be 0', async () => {
+    let result = await cli('blockdate', 'infura_mainnet', '1');
+    expect(result.code).toBe(0);
+  });
+
+  test('Result stdout for mainnet block 1 should contain "Thu Jul 30 2015"', async () => {
+    let result = await cli('blockdate', 'infura_mainnet', '1');
+    expect(result.stdout).toContain('Thu Jul 30 2015');
+  });
 });
