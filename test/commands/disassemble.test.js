a56fbe06a03233cbb2042dc61c219d48c13f1692
Alejandro J. Santander <palebluedot@gmail.com>
2019-05-16 14:37:36 -0300

    Minor updates to tests.

--- test/commands/disassemble.test.js
+++ test/commands/disassemble.test.js
@@ -3,16 +3,17 @@ const cli = require('../utils/cli.js');
 describe('disassemble command', () => {
   test('Should produce appropriate opcode output', async () => {
     const result = await cli('disassemble', './test/artifacts/Test.json');
-    expect(result.stdout).toContain(`171 {0x60} [c253, r213] PUSH1 0x20 (dec 32)
-172 {0x01} [c255, r215] ADD
-173 {0x91} [c256, r216] SWAP2
-174 {0x50} [c257, r217] POP
-175 {0x50} [c258, r218] POP
-176 {0x60} [c259, r219] PUSH1 0x40 (dec 64)
-177 {0x51} [c261, r221] MLOAD
-178 {0x80} [c262, r222] DUP1
-179 {0x91} [c263, r223] SWAP2
-180 {0x03} [c264, r224] SUB
-181 {0x90} [c265, r225] SWAP1`);
+    expect(result.stdout).toContain(`268 {0xa1} [c378, r338] LOG1
+269 {0x65} [c379, r339] PUSH6 0x627a7a723058 (dec 108278179835992)
+270 {0x20} [c386, r346] SHA3
+271 {0x31} [c387, r347] BALANCE
+272 {0x1b} [c388, r348] INVALID (1b)
+273 {0xb8} [c389, r349] INVALID (b8)
+274 {0x54} [c390, r350] SLOAD
+275 {0x78} [c391, r351] PUSH25 0x32dff27412700674e40b06043fe48c222f82044645efb3709d (dec 3.193462532818983e+59)
+276 {0xb2} [c417, r377] INVALID (b2)
+277 {0x86} [c418, r378] DUP7
+278 {0x00} [c419, r379] STOP
+279 {0x2} [c420, r380] INVALID (2)`);
   });
 });
--- test/commands/members.test.js
+++ test/commands/members.test.js
@@ -5,6 +5,7 @@ describe('members command', () => {
   test('Should properly identify the members of Test.sol', async () => {
     const result = await cli('members', './test/artifacts/Test.json');
     expect(result.stdout).toContain(`uint256 public value;
+uint256 public constant CONS;
 constructor() public {...}
 function test(uint256 newValue) public {...}`);
   });
@@ -12,6 +13,7 @@ function test(uint256 newValue) public {...}`);
   test('Should properly identify the members of Test.sol and its ancestors', async () => {
     const result = await cli('members', '--inherited', './test/artifacts/Test.json');
     expect(result.stdout).toContain(`(Test) uint256 public value;
+(Test) uint256 public constant CONS;
 (Test) constructor() public {...}
 (Test) function test(uint256 newValue) public {...}
 (Parent1) uint256 public parent1_value;
