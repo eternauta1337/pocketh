b2640b4f995cd6de0fcd2dfd01ee77d0ea93894f
Alejandro J. Santander <palebluedot@gmail.com>
2019-05-25 20:43:32 -0300

    Revamped test: hex2str

--- test/commands/hex2str.test.js
+++ test/commands/hex2str.test.js
@@ -1,8 +1,15 @@
 const cli = require('../../src/utils/cli.js');
 
-describe('hex2str command', () => {
+describe('hex2uint command', () => {
+
   test('Should properly convert "Hello"', async () => {
     const result = await cli('hex2str', '0x48656c6c6f');
     expect(result.stdout).toContain('Hello');
+  });
+
+  test('Should complain if an invalid hex number is provided', async () => {
+    expect((await cli('hex2uint', 'spongy')).code).toBe(1);
+    expect((await cli('hex2uint', '0.1')).code).toBe(1);
+    expect((await cli('hex2uint', '0xqqq')).code).toBe(1);
   });
 });
