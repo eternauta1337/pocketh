d6144eb329898fb3ba306aca3670356159209a26
Alejandro J. Santander <palebluedot@gmail.com>
2019-05-25 19:59:12 -0300

    Revamped test: calldata.

--- test/commands/calldata.test.js
+++ test/commands/calldata.test.js
@@ -1,19 +1,23 @@
 const cli = require('../../src/utils/cli.js');
 
 describe('calldata command', () => {
+
   test('Should properly identify selectors', async () => {
     const result = await cli('calldata', '0xffffffff');
     expect(result.stdout).toContain('Selector: 0xffffffff');
   });
 
   test('Should properly split parameters', async () => {
+
     const calldata = [
       '0xffffffff',
       '0000000000000000000000000000000000000000000000000000000000000001',
       '0000000000000000000000000000000000000000000000000000000000000002',
       '0000000000000000000000000000000000000000000000000000000000000003'
     ].join('');
+
     const result = await cli('calldata', calldata);
+
     expect(result.stdout).toContain(
       '0x0000: 0x0000000000000000000000000000000000000000000000000000000000000001'
     );
@@ -23,5 +27,10 @@ describe('calldata command', () => {
     expect(result.stdout).toContain(
       '0x0040: 0x0000000000000000000000000000000000000000000000000000000000000003'
     );
+  });
+
+  test('Should complain if an invalid hex value is passed', async () => {
+    const result = await cli('calldata', 'spongy');
+    expect(result.code).toBe(1);
   });
 });
