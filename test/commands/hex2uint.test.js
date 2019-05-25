6255a9cdc5c1f4826dbdc70769f65e04e7677360
Alejandro J. Santander <palebluedot@gmail.com>
2019-05-25 20:41:35 -0300

    Revamped test: hex2uint

--- test/commands/hex2uint.test.js
+++ test/commands/hex2uint.test.js
@@ -1,8 +1,20 @@
 const cli = require('../../src/utils/cli.js');
 
 describe('hex2uint command', () => {
-  test('Should properly convert a small number to hex', async () => {
+
+  test('Should properly convert a small hex to a positive integer', async () => {
     const result = await cli('hex2uint', '0x2a');
     expect(result.stdout).toContain('42');
+  });
+
+  test('Should properly convert a large hex to a positive integer', async () => {
+    const result = await cli('hex2uint', '0xf1f5896ace3a78c347eb7eab503450bc93bd0c3b');
+    expect(result.stdout).toContain('1381342429069413442493521589863962471307977493563');
+  });
+
+  test('Should complain if an invalid hex number is provided', async () => {
+    expect((await cli('hex2uint', 'spongy')).code).toBe(1);
+    expect((await cli('hex2uint', '0.1')).code).toBe(1);
+    expect((await cli('hex2uint', '0xqqq')).code).toBe(1);
   });
 });
