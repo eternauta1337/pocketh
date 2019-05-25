a830b8746b826e7f19f4f02f2cbbf71174b42913
Alejandro J. Santander <palebluedot@gmail.com>
2019-05-25 20:51:03 -0300

    Revamped test: uint2hex

--- test/commands/uint2hex.test.js
+++ test/commands/uint2hex.test.js
@@ -1,8 +1,15 @@
 const cli = require('../../src/utils/cli.js');
 
 describe('uint2hex command', () => {
-  test('Should properly convert a small hex', async () => {
+
+  test('Should properly convert a small positive integer to hex', async () => {
     const result = await cli('uint2hex', '42');
     expect(result.stdout).toContain('0x2a');
+  });
+
+  test('Should complain if an invalid integer is provided', async () => {
+    expect((await cli('int2hex', 'xx')).code).toBe(1);
+    expect((await cli('int2hex', '0.1')).code).toBe(1);
+    expect((await cli('int2hex', '\-1')).code).toBe(1);
   });
 });
