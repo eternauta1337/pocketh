2805625a559984a07fc77a771a058df1c43fa401
Alejandro J. Santander <palebluedot@gmail.com>
2019-05-26 00:38:24 -0300

    Revamped selectors command tests

--- test/commands/selectors.test.js
+++ test/commands/selectors.test.js
@@ -1,8 +1,19 @@
 const cli = require('../../src/utils/cli.js');
 
 describe('selectors command', () => {
-  test('Should produce an expected selector from a know contract', async () => {
-    const result = await cli('selectors', './test/artifacts/Test.json');
+
+  test('Should produce expected selectors from known contract artifacts', async () => {
+    const result = await cli('selectors', 'test/artifacts/Test.json');
     expect(result.stdout).toContain(`0x3fa4f245: value()`);
+    expect(result.stdout).toContain(`0x5dce0fa6: granparent_value()`);
+    expect(result.stdout).toContain(`0xd8175b14: parent1_value()`);
+  });
+
+  test('Should produce expected selectors from a known solidity file', async () => {
+    const result = await cli('selectors', 'test/contracts/Test.sol');
+    expect(result.stdout).toContain(`(compiling test/contracts/Test.sol)`);
+    expect(result.stdout).toContain(`0x3fa4f245: value()`);
+    expect(result.stdout).toContain(`0x5dce0fa6: granparent_value()`);
+    expect(result.stdout).toContain(`0xd8175b14: parent1_value()`);
   });
 });
