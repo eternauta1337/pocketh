9b910a3373a74c7f4da15b485071bd1cbe7adcff
Alejandro J. Santander <palebluedot@gmail.com>
2019-05-26 11:11:22 -0300

    Revamped txs command tests

--- test/commands/txs.test.js
+++ test/commands/txs.test.js
@@ -1,6 +1,60 @@
 const cli = require('../../src/utils/cli.js');
+const getWeb3 = require('../../src/utils/getWeb3.js');
+const fs = require('fs');
 
 describe('txs command', () => {
+
+  let address;
+
+  test('Should find a known number of transactions for a contract deployed in localhost', async () => {
+    
+    // Set up web3.
+    const web3 = await getWeb3('localhost');
+
+    // Retrieve contract artifacts.
+    const artifacts = JSON.parse(fs.readFileSync('test/artifacts/EventEmitter.json', 'utf8'));
+
+    // Set up params for upcoming txs.
+    const accounts = await web3.eth.getAccounts();
+    const params = {
+      from: accounts[0],
+      gas: 1000000,
+      gasPrice: 1
+    };
+
+    // Deploy contract.
+    const contract = new web3.eth.Contract(artifacts.abi);
+    const instance = await contract.deploy({
+      data: artifacts.bytecode
+    }).send(params);
+    address = instance.options.address;
+    // console.log('Deployed contract address:', address);
+
+    // Trigger the contract's emitEventWithValue to create some events.
+    const blockBefore = await web3.eth.getBlockNumber();
+    const txReceipt1 = await instance.methods.emitEventWithValue(42).send(params);
+    const txReceipt2 = await instance.methods.emitEventWithValue(1982).send(params);
+    await instance.methods.anotherMethod().send(params);
+    await instance.methods.anotherMethod().send(params);
+    await instance.methods.anotherMethod().send(params);
+    const blockAfter = await web3.eth.getBlockNumber();
+
+    // Trigger the command to search for events.
+    const result = await cli(
+      'txs', 
+      'localhost',
+      address,
+      '0xc8d8936c',
+      blockBefore,
+      blockAfter
+    );
+
+    // Verifications.
+    expect(result.stdout).toContain(`Found 2 transactions`);
+    expect(result.stdout).toContain(txReceipt1.transactionHash);
+    expect(result.stdout).toContain(txReceipt2.transactionHash);
+  });
+
   test('Should find a known number of transactions from a known contract deployed in mainnet', async () => {
     const result = await cli(
       'txs', 
--- test/contracts/EventEmitter.sol
+++ test/contracts/EventEmitter.sol
@@ -1,8 +1,17 @@
 pragma solidity ^0.5.0;
 
 contract EventEmitter {
+
+  uint256 value;
+
   event Log(uint256 value);
-  function emitEventWithValue(uint256 value) public {
-    emit Log(value);
+
+  function emitEventWithValue(uint256 _value) public {
+    value = _value;
+    emit Log(_value);
+  }
+
+  function anotherMethod() public {
+    value = 0;
   }
 }
