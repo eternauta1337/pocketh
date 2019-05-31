43ff8b0f74c6256698bae1e35e68724f4d48a6c9
Alejandro J. Santander <palebluedot@gmail.com>
2019-05-31 13:50:42 -0300

    Auto compilation now caches output

--- src/utils/getArtifacts.js
+++ src/utils/getArtifacts.js
@@ -2,56 +2,74 @@ const path = require('path');
 const fs = require('fs');
 const tmp = require('tmp');
 const cli = require('../utils/cli');
+const Web3 = require('web3');
+
+let web3;
 
 module.exports = async function(contractPath) {
 
+  // Used for utils only.
+  web3 = new Web3();
+
   // Determine file extension.
-  const dirname = path.dirname(contractPath);
+  let dirname = path.dirname(contractPath);
   const filename = path.basename(contractPath);
   const comps = filename.split('.');
   const name = comps[0];
   const ext = comps[1];
 
-  // If the extension is json, just load the file and return its contents.
-  if(ext === 'json') return {
-    artifacts: retrieveJsonArtifacts(contractPath),
-    basedir: dirname
-  };
-  // If the extension is sol, compile the contract
-  // and then return the json artifacts.
-  if(ext === 'sol') {
-  
-    // Create a temporary directory to store the artifacts in.
-    const tmpdir = tmp.dirSync();
-    console.log(`Auto compiling ${contractPath} to ${tmpdir.name}/`);
+  // Validate path.
+  if(ext !== 'sol' && ext !== 'json')
+    throw new Error(`Unrecognized extension ${ext}`);
 
-    // Compile the file.
-    // Wrap it in a try/catch to avoid cancelling the auto compilation
-    // for trivial errors that solcjs sometimes throws.
-    let result;
-    try {
-      result = await cli(
-        'compile', 
-        contractPath, 
-        `${tmpdir.name}/`
-      );
-      
-      // Return the compiled artifacts.
-      const compiledPath = `${tmpdir.name}/${name}.json`;
-      return {
-        artifacts: retrieveJsonArtifacts(compiledPath),
-        basedir: tmpdir.name
-      };
-    }
-    catch(error) {
-      // If the error has to do with the json file, then something
-      // really did go wrong. In that case, do report the compilation error.
-      if(error.message.includes('Cannot find')) throw new Error(`Unable to auto compile ${contractPath}: ${result.error}`);
-    }
+  // Auto compile?
+  let compiledPath = contractPath;
+  if(ext === 'sol') {
+    dirname = await compileSolidityFile(contractPath);
+    compiledPath = `${dirname}/${name}.json`;
   }
 
-  throw new Error(`Unrecognized extension ${ext}`);
+  // Return a wrapper with the artifacts
+  // and a reference directory for where they're stored.
+  return {
+    artifacts: retrieveJsonArtifacts(compiledPath),
+    basedir: dirname
+  };
 };
+
+async function compileSolidityFile(sourcePath) {
+    
+  // Create a temporary directory to store the artifacts in.
+  // If the directory already exists, then its artifacts can be reused.
+  const tmpdir = `/tmp/${getSourceCodeHash(sourcePath)}`;
+  if(fs.existsSync(tmpdir)) return tmpdir;
+  else fs.mkdirSync(tmpdir);
+  console.log(`Auto compiling ${sourcePath} to ${tmpdir}/`);
+
+  // Compile the file.
+  // Wrap it in a try/catch to avoid cancelling the auto compilation
+  // for trivial errors that solcjs sometimes throws.
+  let result;
+  try {
+    result = await cli(
+      'compile', 
+      sourcePath, 
+      tmpdir
+    );
+    
+    return tmpdir;
+  }
+  catch(error) {
+    // If the error has to do with the json file, then something
+    // really did go wrong. In that case, do report the compilation error.
+    if(error.message.includes('Cannot find')) throw new Error(`Unable to auto compile ${sourcePath}: ${result.error}`);
+  }
+}
+
+function getSourceCodeHash(sourcePath) {
+  const source = fs.readFileSync(sourcePath, 'utf8');
+  return web3.utils.sha3(source).substring(2, 14);
+}
 
 function retrieveJsonArtifacts(path) {
   if(!fs.existsSync(path)) throw new Error(`Cannot find ${path}`);
